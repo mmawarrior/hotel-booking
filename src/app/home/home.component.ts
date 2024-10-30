e1054f88d34d5f6e24c6c3cf59d696761e5e6554
Dat Nguyen <datmmawarrior@gmail.com>
2024-10-30 22:07:16 +0100

    Update site content or styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'; // Voeg deze import toe
 import { RoomsComponent } from '../rooms/rooms.component'; 
 import { KaisekiComponent } from '../kaiseki/kaiseki.component'; 
 import { SightseeingModule } from '../sightseeing/sightseeing.module'; // Zorg ervoor dat je de module importeert
 import { AccessComponent } from '../access/access.component';
 import { FooterModule } from '../footer/footer.module'; // Zorg ervoor dat het pad klopt
-import { NavigationComponent } from '../navigation/navigation.component'; // Adjust the path accordingly
+import { NavigationComponent } from '../navigation/navigation.component';
 
 @Component({
   selector: 'app-home',
@@ -14,5 +15,9 @@ import { NavigationComponent } from '../navigation/navigation.component'; // Adj
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  // Add any logic specific to the HomeComponent here
+  constructor(private router: Router) {} // Zorg ervoor dat 'Router' is toegevoegd aan de constructor
+
+  goToReserve() {
+    this.router.navigate(['/reserve']);
+  }
 }
